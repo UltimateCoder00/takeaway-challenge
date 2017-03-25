4ceb810aa87d5f4206a61de160b9b5ff7def9857
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-25 07:16:05 +0000

    Add print method

--- lib/menu.rb
+++ lib/menu.rb
@@ -1,8 +1,34 @@
+require 'csv'
+
 class Menu
-  attr_reader :food, :price
+  attr_reader :food, :price, :list
 
   def initialize(food, price)
     @food = food
     @price = price
+    @list
+  end
+
+  def print
+
+    file_path = Dir.pwd + '/lib/menu_list.csv'
+
+    file = File.new(file_path)
+    @list = CSV.read(file)
+
+    max_length = 0
+    list.each do |food, price|
+      max_length = food.length if max_length <= food.length
+    end
+
+    counter = 1
+    spacing = 4
+
+    puts "No".to_s.ljust(spacing) + "Food".ljust(max_length + spacing) + "Price"
+    puts "*"*(max_length*(spacing / 2 ))
+    list.each do |food, price|
+      puts counter.to_s.ljust(spacing) + food.to_s.ljust(max_length + spacing) + "£" + price
+      counter += 1
+    end
   end
 end
