38902e14206fbfd42be88e8c7c33468c13911b07
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-26 10:21:30 +0100

    Test #add & #remove methods

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -17,8 +17,21 @@ describe Menu do
   end
 
   describe 'Print menu' do
-    it '#load' do
-      expect(menu.load_list).to eq menu.list
+    it '#load_list' do
+      expect(menu.list).to eq menu.list
+    end
+  end
+
+  describe 'Add food to menu' do
+    it '#add' do
+      expect(menu.add(food, price)).to eq "#{price}"
+    end
+  end
+
+  describe 'Remove food from menu' do
+    it '#remove' do
+      menu.add(food, price)
+      expect(menu.remove(food)).to eq "#{price}"
     end
   end
 end
