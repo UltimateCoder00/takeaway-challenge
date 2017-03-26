4bce9a660a8f06e7758999f8fd4db6785b78976e
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-26 15:18:52 +0100

    Add #order, #checkout and send_sms methods and refactor

--- lib/takeaway.rb
+++ lib/takeaway.rb
@@ -2,31 +2,50 @@ require_relative 'menu'
 
 class Takeaway
 
-  attr_reader :menu_class
+  attr_reader :show_menu, :basket, :sms
 
-  def initialize(menu_class: Menu.new(nil,nil))
-    @menu_class = menu_class
-    @menu_list
+  def initialize(menu_class: Menu.new, sms_service: SMS.new)
+    @show_menu = menu_class.list
+    @sms = sms_service
+    @basket = {}
   end
 
-  def display_menu
-    menu_list = menu_class.load_list
-
-
-    max_length = 0
-    menu_list.each do |food, price|
-      max_length = food.length if max_length <= food.length
+  def order(food)
+    if @basket.has_key?("#{food}")
+      @basket["#{food}"] += @show_menu["#{food}"].to_f
+    else
+      @basket["#{food}"] = @show_menu["#{food}"].to_f
     end
 
-    counter = 1
-    spacing = 4
-
-    puts "No".to_s.ljust(spacing) + "Food".ljust(max_length + spacing) + "Price"
-    puts "*"*(max_length*(spacing / 2 ))
-    menu_list.each do |food, price|
-      puts counter.to_s.ljust(spacing) + food.to_s.ljust(max_length + spacing) + "£" + price
-      counter += 1
-    end
-
+    puts "Your current basket has: #{basket}"
   end
+
+  def checkout
+    while true
+      puts @basket
+      sum = 0
+      basket.each { |key, value| sum += value }
+      puts "Your current balance is: £#{'%.2f' % sum}"
+      puts "Please confirm your order is correct (Y/N)"
+      reply = gets.chomp
+
+      if reply.upcase == 'Y'
+        puts "You will shortly receive a confirmation text message for your order"
+        send_sms
+        break
+      elsif reply.upcase == 'N'
+        puts "Your order is cancelled"
+        @basket.clear
+        break
+      else
+        next
+      end
+    end
+  end
+
+  def send_sms
+    message = "Your order will be with you soon"
+    # sms.send(message)
+  end
+
 end
