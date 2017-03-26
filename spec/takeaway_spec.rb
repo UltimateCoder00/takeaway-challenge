5117eb9d6e6627ce401c89fcc1bd59f5fdddc802
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-26 15:20:29 +0100

    Add #order and #checkout method tests

--- spec/takeaway_spec.rb
+++ spec/takeaway_spec.rb
@@ -2,9 +2,24 @@ require 'takeaway'
 
 describe Takeaway do
   subject(:takeaway) {described_class.new}
-  let(:menu_class) {Menu.new(nil,nil)}
 
-  it '#display_menu' do
-    expect(takeaway.display_menu).to eq menu_class.load_list
+  let(:sms) { double(:sms, send: message) }
+  let(:message) { "Your order will be with you soon" }
+
+  let(:food) {"Cod"}
+  let(:food2) {"Haddock"}
+  let(:food3) {"Calamari"}
+  let(:food4) {"Cod"}
+
+  it '#order' do
+    expect(takeaway.order(food)).to eq nil
+  end
+
+  it '#checkout' do
+    takeaway.order(food)
+    takeaway.order(food2)
+    takeaway.order(food3)
+    takeaway.order(food4)
+    expect(takeaway.checkout).to eq nil
   end
 end
