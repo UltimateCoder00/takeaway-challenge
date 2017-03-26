0a9d6ccabf753cff379ad66a69ed3608ba53cfb5
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-26 05:49:07 +0100

    Change #print method to #load_list and modify

--- lib/menu.rb
+++ lib/menu.rb
@@ -9,26 +9,10 @@ class Menu
     @list
   end
 
-  def print
-
+  def load_list
     file_path = Dir.pwd + '/lib/menu_list.csv'
 
     file = File.new(file_path)
     @list = CSV.read(file)
-
-    # max_length = 0
-    # list.each do |food, price|
-    #   max_length = food.length if max_length <= food.length
-    # end
-    #
-    # counter = 1
-    # spacing = 4
-    #
-    # puts "No".to_s.ljust(spacing) + "Food".ljust(max_length + spacing) + "Price"
-    # puts "*"*(max_length*(spacing / 2 ))
-    # list.each do |food, price|
-    #   puts counter.to_s.ljust(spacing) + food.to_s.ljust(max_length + spacing) + "£" + price
-    #   counter += 1
-    # end
   end
 end
