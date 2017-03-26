c0ffd80b4190eefcdd7e618d44da358671f497ad
Hasan Sonmez <hasansonmez100@hotmail.com>
2017-03-26 16:54:50 +0100

    Refactor, add #update method

--- spec/menu_spec.rb
+++ spec/menu_spec.rb
@@ -6,26 +6,39 @@ describe Menu do
   let(:food) {:food}
   let(:price) {:food_price}
 
-  describe 'Print menu' do
-    it '#list' do
+  let(:food2) {"Cod"}
+  let(:price2) {"5.00"}
+
+  describe '#list' do
+    it 'Print menu' do
       expect(menu.list).to eq menu.list
     end
   end
 
-  describe 'Add food to menu' do
-    it '#add' do
-      expect(menu.add(food, price)).to eq "#{price}"
+  describe '#add' do
+    it 'Add new food to menu' do
+      expect(menu.add(food, price)).to eq nil
+    end
+
+    it 'Change existing foods price in menu' do
+      expect(menu.add(food2, price2)).to eq nil
     end
   end
 
-  describe 'Remove food from menu' do
-    it '#remove' do
+  describe '#remove' do
+    it 'Remove existing food from menu' do
       menu.add(food, price)
-      expect(menu.remove(food)).to eq "#{price}"
+      expect(menu.remove(food)).to eq nil
     end
 
-    it 'Removing item that does not exist in the list' do
-      expect(menu.remove(food)).to eq nil
+    it 'Removing food item that does not exist in the list' do
+      expect{menu.remove(food)}.to raise_error "Error: The food item you provided does not exist in the menu list"
+    end
+  end
+
+  describe '#update' do
+    it 'Update menu list file' do
+      expect(menu.update).to eq menu.list
     end
   end
 end
